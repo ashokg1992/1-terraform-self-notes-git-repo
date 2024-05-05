557fdaee839086347faec17e70955298d14880ae
ashok <ashokreddyothers@gmail.com>
2024-05-05 18:23:22 +0530

    add

--- modules/aws_vpc/variable.tf
+++ modules/aws_vpc/variable.tf
@@ -1,7 +1,11 @@
 variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+
+
+}
+variable "tags" {
   
 }
-
-variable "tags" {
+variable "availability_zone" {
   
 }
---
+++ modules/subnets/variable.tf
@@ -0,0 +1,12 @@
+ variable "subnet_cidr_block"{
+
+}
+variable "vpc_id" {
+
+}
+variable"availability_zone" {
+
+}
+variable "tags" {
+  
+}
